54c1c9453eb36c267a5993d7e148a14846d6faa3
SeekerOfTheTruth <94625331+SeekerOfTheTruth@users.noreply.github.com>
2022-04-16 17:51:11 +1000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Project 1`,
+    siteTitleAlt: `Boy Blo Nature`,
+    siteHeadline: `Id like to use Drupal as a Data Source`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `I am just getting started with Gatbsy and GitHub as well`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `SeekerOfTheTruth`,
   },
   plugins: [
     {
